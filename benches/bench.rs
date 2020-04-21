baceaccabb0a49235fa21bcdb706f11c138a2a5c
Maiko Mueller <maiko.mueller@web.de>
2020-04-21 11:06:48 +0200

    Fix bench errors from previous commit

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,7 +2,6 @@ use criterion::Bencher;
 use criterion::BenchmarkGroup;
 use criterion::*;
 use mergesort::{mergesort, steal};
-// use rayon_logs::prelude::*;
 use rayon::prelude::*;
 use rayon_adaptive::adaptive_sort;
 #[macro_use]
@@ -12,17 +11,16 @@ lazy_static! {
         .take(2usize.pow(21))
         .map(|x: u32| x % 1_000_000)
         .collect();
-    // static ref checksum: usize = V.iter().sum::<u32>() as usize;
+    static ref CHECKSUM: u32 = V.iter().sum::<u32>();
 }
 
 fn verify(numbers: &Vec<u32>) {
-    // assert_eq!(numbers.iter().sum::<u32>(), *checksum);
+    assert_eq!(numbers.iter().sum::<u32>(), *CHECKSUM);
     assert!(numbers.windows(2).all(|w| w[0] <= w[1]));
 }
 
 pub fn adaptive(group: &mut BenchmarkGroup<criterion::measurement::WallTime>) {
     for steal_counter in &[1, 2, 4, 6, 8, 10] {
-        // for size in &[1, 2, 3, 4] {
         for size in &[2, 4, 6, 8, 12, 16, 20, 24, 32] {
             group.bench_with_input(
                 BenchmarkId::new(format!("Adaptive_{}", steal_counter), size),
@@ -76,7 +74,7 @@ pub fn rayon_adaptive(group: &mut BenchmarkGroup<criterion::measurement::WallTim
 }
 pub fn comparison() {
     let pool = rayon_logs::ThreadPoolBuilder::new()
-        .num_threads(4)
+        // .num_threads(4)
         .steal_callback(move |x| steal::steal(8, x))
         .build()
         .unwrap();
@@ -138,32 +136,16 @@ pub fn single(b: &mut Bencher) {
 }
 
 fn bench(c: &mut Criterion) {
-    println!("{}", V[0]);
+    println!("{}", V[0]); // to init lazy_static
     let mut group = c.benchmark_group("MergeSorting");
     group.measurement_time(std::time::Duration::new(3, 0));
     group.warm_up_time(std::time::Duration::new(1, 0));
     group.sample_size(10);
 
-    rayon_adaptive(&mut group);
-    // group.bench_function("single", |mut b: &mut Bencher| {
-    //     single(&mut b);
-    // });
     adaptive(&mut group);
+    rayon_adaptive(&mut group);
     iterator(&mut group);
     // comparison();
-
-    /*
-    group.bench_function("iterator", |mut b: &mut Bencher| {
-        iterator(&mut b);
-    });
-    */
-
-    /*
-    group.bench_function("perfect split", |b| {
-        perfect_split(b);
-    });
-    */
-    // merge_speed_test(&mut group);
 
     group.finish();
 }
